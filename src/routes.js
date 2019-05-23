3cde6780c029f5bb262e8030bd9e36ec0e55d7a2
Derek Sams <dereksams@Dereks-MacBook-Pro.local>
2019-05-23 17:10:11 -0500

    adding routes

---
+++ src/routes.js
@@ -0,0 +1,12 @@
+import React from "react";
+import { Switch, Route } from "react-router-dom";
+import { loggedOutHome } from "./components/loggedOutHome/loggedOutHome";
+import Login from "./components/Login";
+
+// hey
+export default (
+  <Switch>
+    <Route path="/login" component={Login} />
+    <Route exact path="/" component={loggedOutHome} />
+  </Switch>
+);
