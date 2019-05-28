46e3dd984a346c020c0e1d0d2f14bd2d579ad7db
Bryan Hawkins <bhawkins3729@gmail.com>
2019-05-28 11:51:32 -0500

    made side bar

--- src/App.js
+++ src/App.js
@@ -1,15 +1,16 @@
 import React from "react";
-import "./App.css";
+import "../src/sidebar.js/sidebar.scss";
 import { HashRouter } from "react-router-dom";
 import routes from "./routes";
 import Navbar from './components/navbar/navbar';
-
+import SideBar from '../src/sidebar.js/sidebar.js';
 
 function App() {
   return (
     <HashRouter>
       <div className="App">
         <header className="App-header" />
+        <SideBar />
         <Navbar />
         {routes}
       </div>
