3fee3d411dc12a11c62fcc3d1d9bf1564339fc4b
Gilbert Baracka <gibbs@Gilberts-MacBook-Air.local>
2019-05-28 11:32:38 -0500

    restuarants

---
+++ src/components/restuarants/Restaurants.js
@@ -0,0 +1,18 @@
+import React, { useState } from "react";
+
+function Restuarants() {
+  return (
+    <div>
+      <h1>Categories</h1>
+      <h2>Filtered</h2>
+      <h3>Promotional</h3>
+      <h4>Featured</h4>
+      <h4>firstorder</h4>
+      <h4>favorites</h4>
+      <h4>new</h4>
+      <h5>all</h5>
+    </div>
+  );
+}
+
+export default Restuarants;
