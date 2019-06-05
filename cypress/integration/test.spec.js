f924fc88402d71af263912ec81b8cbd262546ea8
Bryan Hawkins <bhawkins3729@gmail.com>
2019-06-05 16:53:27 -0500

    tests done and warning for key props gone

--- cypress/integration/test.spec.js
+++ cypress/integration/test.spec.js
@@ -6,37 +6,29 @@ describe('First Test', () => {
     });
 });
 
-describe("Second Test", () => {
+describe("Our app should open in localhost", () => {
     it("Visit the app", () => {
         cy.visit("/");
     });
 });
 
-describe("Second Test", () => {
+describe("Routing to login page test", () => {
     it("Click sign in", () => {
         cy.visit("/login");
     });
 });
 
-describe("Second Test", () => {
+describe("Routing to signup page test", () => {
     it("Click sign up", () => {
         cy.visit("/signup");
     });
 });
 
-describe("Second Test", () => {
+describe("Routing to sign out page test ", () => {
     it("Click sign out in sidebar  ", () => {
         cy.visit("/signout#/");
     });
 });
 
-//unit tests bryan
 
-describe('Examining the syntax of Jest tests', () => {
-
-    it('sums numbers', () => {
-        expect(1 + 2).toEqual(3);
-        expect(2 + 2).toEqual(4);
-    });
-});
 
