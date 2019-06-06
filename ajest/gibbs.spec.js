fd9ff1833559b5511bead83eda50c2831721f1b0
Gilbert Baracka <gibbs@gilberts-air.attlocal.net>
2019-06-05 22:01:59 -0500

    test

--- ajest/gibbs.spec.js
+++ ajest/gibbs.spec.js
@@ -18,5 +18,30 @@ describe("test post methods", () => {
   });
 });
 
-//// unit test
-// others in crypress
+const Restaurants = resquire("../src/components/restaurants");
+const axios = require("axios")
+
+jest.mock("axios");
+describe("restaurants should have get list of restuarants near", () => {
+  it("shows a list restuarants", async () => {
+    describe("test post methods", () => {
+      test("It should respond with status 200", () => {
+        return request(app)
+          .post("/api/restaurant")
+          .expect(200);
+      }).then(res => {
+        
+      
+   
+    axios.get.mockResolvedValue(res);
+    const component = create(<Restaurants />);
+    const instance = component.getInstance();
+    await instance.componentDidMount();
+    const root = component.root;
+    const listOfNames = root.findAll(element => element.type === "h3");
+    expect(listOfNames[0].props.children).toBe("article");
+    expect(listOfNames[1].props.children).toBe("section");
+  })
+});
+  });
+});
