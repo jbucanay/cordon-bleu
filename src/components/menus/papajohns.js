9b045e114762835ddc4c450c1325657f4c817594
Bryan Hawkins <bhawkins3729@gmail.com>
2019-06-06 13:42:34 -0500

    styled each restaurant page

--- src/components/menus/papajohns.js
+++ src/components/menus/papajohns.js
@@ -4,6 +4,7 @@ import './menus.scss';
 
 
 
+
 export default class papajohns extends Component {
     constructor() {
         super()
@@ -41,7 +42,7 @@ export default class papajohns extends Component {
         let itemList = this.state.items.map((menu, index) => {
             return <div className="food-item" key={index}>
                 <h3>{menu.name}</h3>
-                <h5>{menu.description}</h5>
+                <h5 className="menu-description">{menu.description && menu.description.length > 80 ? menu.description.slice(0, 80) + "..." : menu.description}</h5>
                 <h4> $ {menu.price}.00</h4>
                 <button className="addToCart" onClick={() => {
                     this.handleClick(menu.id)
@@ -53,6 +54,29 @@ export default class papajohns extends Component {
         })
         return (
             <div>
+                <div className="menu-items-upper">
+                    <img className="menu-logo" src="https://upload.wikimedia.org/wikipedia/en/thumb/c/c4/Papa_John%27s_Pizza_logo.svg/1280px-Papa_John%27s_Pizza_logo.svg.png" alt="logo" />
+
+                    <h1 className="restaurant-name">Papa John's Pizza</h1>
+                    <h3 className="gray-tag">Open Hours: 9:30AM - 9:10AM</h3>
+                    <div class="rating">
+                        <span>&#9733;</span><span>&#9733;</span><span>&#9733;</span><span>&#9733;</span><span>☆</span>
+                        <h3 className="gray-tag">4.6 (24 Ratings)</h3>
+                    </div>
+                    <div className="info-part">
+                        <div className="seperate-part"> <div className="top">Free</div><div className="bottom">delivery</div></div>
+                        <div className="seperate-part"> <div className="top">15-25</div><div className="bottom">minutes</div></div>
+                        <div className="seperate-part"> <div className="top">0.7</div><div className="bottom">miles</div></div>
+                    </div>
+
+                </div>
+                <div className="item-bar">
+                    <span className="item-bar-word">Pizza</span>
+                    <span className="item-bar-word">Sides </span>
+                    <span className="item-bar-word">Desserts</span>
+                    <span className="item-bar-word">Drinks </span>
+                    <span className="item-bar-word">Extras </span>
+                </div>
 
                 <div className="food-item-outer">
 
--- src/components/menus/wendys.js
+++ src/components/menus/wendys.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react';
 import axios from 'axios';
 import './menus.scss';
+import checkmark from '../../images/checkmark.png'
+
 
 
 
@@ -41,7 +43,7 @@ export default class wendys extends Component {
         let itemList = this.state.items.map((menu, index) => {
             return <div className="food-item" key={index}>
                 <h3>{menu.name}</h3>
-                <h5>{menu.description}</h5>
+                <h5 className="menu-description">{menu.description && menu.description.length > 80 ? menu.description.slice(0, 80) + "..." : menu.description}</h5>
                 <h4> $ {menu.price}.00</h4>
                 <button className="addToCart" onClick={() => {
                     this.handleClick(menu.id)
@@ -53,6 +55,30 @@ export default class wendys extends Component {
         })
         return (
             <div>
+                <div className="menu-items-upper">
+                    <img className="menu-logo" src="https://amp.businessinsider.com/images/51e3f458eab8ea135800001d-750-606.jpg" alt="logo" />
+
+                    <h2 className="delivery-tag"><img className="checkmark" src={checkmark} alt="checkmark" />FREE DELIVERY WITH DASHPASS</h2>
+                    <h1 className="restaurant-name">Wendy's</h1>
+                    <h3 className="gray-tag">Open Hours: 10:00AM - 10:30AM</h3>
+                    <div class="rating">
+                        <span>&#9733;</span><span>&#9733;</span><span>&#9733;</span><span>&#9733;</span><span>☆</span>
+                        <h3 className="gray-tag">4.5 (769 Ratings)</h3>
+                    </div>
+                    <div className="info-part">
+                        <div className="seperate-part"> <div className="top">Free</div><div className="bottom">delivery</div></div>
+                        <div className="seperate-part"> <div className="top">26-36</div><div className="bottom">minutes</div></div>
+                        <div className="seperate-part"> <div className="top">1.8</div><div className="bottom">miles</div></div>
+                    </div>
+
+                </div>
+                <div className="item-bar">
+                    <span className="item-bar-word">Entrees </span>
+                    <span className="item-bar-word">Salads</span>
+                    <span className="item-bar-word">Sides </span>
+                    <span className="item-bar-word">Beverages </span>
+                    <span className="item-bar-word">Frosty </span>
+                </div>
 
                 <div className="food-item-outer">
 
